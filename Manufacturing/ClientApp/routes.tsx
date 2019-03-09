d41b3a73d94476f1eec1e3561359902d8057b444
Sean Martin <sean.martin@touche.social>
2019-03-09 09:51:17 +0000

    Removed unused files

--- Manufacturing/ClientApp/routes.tsx
+++ Manufacturing/ClientApp/routes.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import Home from './components/Home';
-import FetchData from './components/FetchData';
-import Counter from './components/Counter';
 import MaterialListView from './components/MaterialListView';
 import { ModalContainer } from 'react-router-modal';
 import MaterialView from './components/MaterialView';
